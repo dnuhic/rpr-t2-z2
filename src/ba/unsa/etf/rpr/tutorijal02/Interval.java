cce5e2fe4bcd44eae005590b648724035c59c8eb
dnuhic <dnuhic1@etf.unsa.ba>
2020-11-02 19:30:05 +0100

    Napravljen konstruktor bez parametara.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -4,6 +4,11 @@ public class Interval {
     private double pocetnaT, krajnjaT;
     private boolean PocPripada, KrPripada;
 
+    public Interval() {
+        pocetnaT = 0; krajnjaT = 0;
+        PocPripada = false; KrPripada = false;
+    }
+
     public Interval(double pocetnaT, double krajnjaT, boolean pocPripada, boolean krPripada) {
         this.pocetnaT = pocetnaT;
         this.krajnjaT = krajnjaT;
