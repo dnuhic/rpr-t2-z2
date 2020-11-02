bc54e20ee49698edfc75a19e1c1fd32a4d1c4848
dnuhic <dnuhic1@etf.unsa.ba>
2020-11-02 19:38:33 +0100

    Napravljene metode isIn i isNull.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -18,6 +18,25 @@ public class Interval {
                 throw new IllegalArgumentException("Neispravna pocetna ili kranja tacka.");
     }
 
+    public boolean isNull() {
+        return pocetnaT == 0 && krajnjaT == 0 && !PocPripada && !KrPripada;
+    }
+
+    public boolean isIn(double t) {
+        if(PocPripada && KrPripada)
+            if(t >= pocetnaT && t <= krajnjaT) return true;
+            else return false;
+        else if(PocPripada && !KrPripada)
+            if(t >= pocetnaT && t < krajnjaT) return true;
+            else return false;
+        else if(KrPripada && !PocPripada)
+            if(t > pocetnaT && t <= krajnjaT) return true;
+            else return false;
+        else if(!KrPripada && !PocPripada)
+            if(t > pocetnaT && t < krajnjaT) return true;
+            else return false;
+    }
+
 
 
 }
