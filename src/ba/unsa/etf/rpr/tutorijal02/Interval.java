ddbb5b89b209e987a8a0292d2a50716a6d6e3a8a
dnuhic <dnuhic1@etf.unsa.ba>
2020-11-02 19:28:05 +0100

    Napravljen konstruktor klase Interval.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,18 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetnaT, krajnjaT;
+    private boolean PocPripada, KrPripada;
+
+    public Interval(double pocetnaT, double krajnjaT, boolean pocPripada, boolean krPripada) {
+        this.pocetnaT = pocetnaT;
+        this.krajnjaT = krajnjaT;
+        PocPripada = pocPripada;
+        KrPripada = krPripada;
+        if(this.pocetnaT > this.krajnjaT)
+                throw new IllegalArgumentException("Neispravna pocetna ili kranja tacka.");
+    }
+
 
 
 }
