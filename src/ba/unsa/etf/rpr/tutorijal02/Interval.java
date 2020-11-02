1355b21674d25798c34a5b5de9f466207c51c8fa
dnuhic <dnuhic1@etf.unsa.ba>
2020-11-02 19:57:35 +0100

    Napravljena metoda clanica intersect i staticka metoda intersect.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
+import static java.lang.Math.abs;
+
 public class Interval {
     private double pocetnaT, krajnjaT;
     private boolean PocPripada, KrPripada;
@@ -32,11 +34,29 @@ public class Interval {
         else if(KrPripada && !PocPripada)
             if(t > pocetnaT && t <= krajnjaT) return true;
             else return false;
-        else if(!KrPripada && !PocPripada)
+        else
             if(t > pocetnaT && t < krajnjaT) return true;
             else return false;
     }
 
+    public Interval intersect(Interval i) {
+        Interval temp = new Interval();
+        //if(abs(pocetnaT - i.pocetnaT) <= 10e-7 &)
+        if(pocetnaT > i.pocetnaT) temp.pocetnaT = pocetnaT;
+        else temp.pocetnaT = i.pocetnaT;
+        if(krajnjaT < i.krajnjaT) temp.krajnjaT = krajnjaT;
+        else temp.pocetnaT = i.krajnjaT;
+        return temp;
+    }
 
+    public static Interval intersect(Interval i1, Interval i2) {
+        Interval temp = new Interval();
+        //if(abs(pocetnaT - i.pocetnaT) <= 10e-7 &)
+        if(i1.pocetnaT > i2.pocetnaT) temp.pocetnaT = i1.pocetnaT;
+        else temp.pocetnaT = i2.pocetnaT;
+        if(i1.krajnjaT < i2.krajnjaT) temp.krajnjaT = i1.krajnjaT;
+        else temp.pocetnaT = i2.krajnjaT;
+        return temp;
+    }
 
 }
